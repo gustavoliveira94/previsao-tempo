b6a381071faf2bd14850406fa6139f69e58e8680
Gustavo <gmacedo@mcontigo.com>
2020-10-10 14:26:34 -0300

    refactor: change api images

---
+++ src/utils/api-image.js
@@ -0,0 +1,7 @@
+import Unsplash from 'unsplash-js';
+
+export const unsplash = new Unsplash({
+    accessKey:
+        '640b696bce9f9fd1916532502c42a90c3e6bee2e598b0d718f1afabf9403e414',
+    secret: 'b70859e0f8fafe7558a6b7ec9f1cdd3ba9ce5912f525a9fee0c383b78fd5f05f',
+});
